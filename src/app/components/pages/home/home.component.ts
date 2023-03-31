685c442878033d2d459c21ae8461eee5da422bcb
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2023-03-31 13:25:12 -0300

    implementei um campo de busca na home onde o usuário pode fazer um filtro pelo momento que queira

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -14,6 +14,8 @@ export class HomeComponent implements OnInit {
   moments: Moment[] = [];
   baseApiUrl = environment.baseApiUrl;
 
+  searchTerm: string = '';
+
   constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
@@ -29,5 +31,14 @@ export class HomeComponent implements OnInit {
       this.allMoments = data;
       this.moments = data;
     });
+  }
+
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) =>
+      moment.title.toLocaleLowerCase().includes(value)
+    );
   }
 }
